9fe1a96ceae9738618f21d2c2d1fe23c536908fe
Ricardo Mota <ricardosllm@gmail.com>
2018-01-04 09:17:59 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Emacs
+brew tap d12frosted/emacs-plus
+brew install emacs-plus
+brew linkapps emacs-plus
 # clone spacemacs
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
@@ -16,12 +19,11 @@ ln -s $PWD/.config/fish/config.fish ~/.config/fish/config.fish
 ln -s $PWD/.spacemacs ~/.spacemacs
 ln -s $PWD/.emacs.d/private/snippets ~/.emacs.d/private/snippets
 
+# Hammerspoon config
+ln -s $PWD/.hammerspoon/init.lua ~/.hammerspoon/init.lua
+
 emacs --insecure 
 
 # Source code font 
 # https//github.com/adobe-fonts/source-code-pro
 
-# Todo.txt
-brew install todo-txt
-rm ~/.todo.cfg
-ln -s $PWD/.todo.cfg ~/.todo.cfg
